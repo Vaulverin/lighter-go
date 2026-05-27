7ac2a32b2dd1d60760380ec03e381edbb95ae1ca
Александр Ваулин <vauulin@gmail.com>
2026-05-27 15:17:17 +0400

    fix: export v2 wrappers from windows dll

--- sharedlib/sign_info_v2.c
+++ sharedlib/sign_info_v2.c
@@ -2,13 +2,13 @@
 
 #include "signal_stack.h"
 
-int SignCreateOrderInfoV2(int cMarketIndex, long long cClientOrderIndex, long long cBaseAmount,
-                          int cPrice, int cIsAsk, int cOrderType, int cTimeInForce,
-                          int cReduceOnly, int cTriggerPrice, long long cOrderExpiry,
-                          uint8_t cSkipNonce, long long cNonce, int cApiKeyIndex,
-                          long long cAccountIndex, char* txInfoBuffer, int txInfoCapacity,
-                          int* txInfoLength, char* errorBuffer, int errorCapacity,
-                          int* errorLength) {
+LIGHTER_SIGN_INFO_V2_EXPORT int SignCreateOrderInfoV2(int cMarketIndex, long long cClientOrderIndex, long long cBaseAmount,
+                                                      int cPrice, int cIsAsk, int cOrderType, int cTimeInForce,
+                                                      int cReduceOnly, int cTriggerPrice, long long cOrderExpiry,
+                                                      uint8_t cSkipNonce, long long cNonce, int cApiKeyIndex,
+                                                      long long cAccountIndex, char* txInfoBuffer, int txInfoCapacity,
+                                                      int* txInfoLength, char* errorBuffer, int errorCapacity,
+                                                      int* errorLength) {
     lighter_ensure_large_sigaltstack();
     return SignCreateOrderInfoV2Go(cMarketIndex, cClientOrderIndex, cBaseAmount,
                                    cPrice, cIsAsk, cOrderType, cTimeInForce,
@@ -18,10 +18,10 @@ int SignCreateOrderInfoV2(int cMarketIndex, long long cClientOrderIndex, long lo
                                    errorBuffer, errorCapacity, errorLength);
 }
 
-int SignCancelOrderInfoV2(int cMarketIndex, long long cOrderIndex, uint8_t cSkipNonce,
-                          long long cNonce, int cApiKeyIndex, long long cAccountIndex,
-                          char* txInfoBuffer, int txInfoCapacity, int* txInfoLength,
-                          char* errorBuffer, int errorCapacity, int* errorLength) {
+LIGHTER_SIGN_INFO_V2_EXPORT int SignCancelOrderInfoV2(int cMarketIndex, long long cOrderIndex, uint8_t cSkipNonce,
+                                                      long long cNonce, int cApiKeyIndex, long long cAccountIndex,
+                                                      char* txInfoBuffer, int txInfoCapacity, int* txInfoLength,
+                                                      char* errorBuffer, int errorCapacity, int* errorLength) {
     lighter_ensure_large_sigaltstack();
     return SignCancelOrderInfoV2Go(cMarketIndex, cOrderIndex, cSkipNonce, cNonce,
                                    cApiKeyIndex, cAccountIndex, txInfoBuffer,
@@ -29,11 +29,11 @@ int SignCancelOrderInfoV2(int cMarketIndex, long long cOrderIndex, uint8_t cSkip
                                    errorCapacity, errorLength);
 }
 
-int SignModifyOrderInfoV2(int cMarketIndex, long long cIndex, long long cBaseAmount,
-                          long long cPrice, long long cTriggerPrice, uint8_t cSkipNonce,
-                          long long cNonce, int cApiKeyIndex, long long cAccountIndex,
-                          char* txInfoBuffer, int txInfoCapacity, int* txInfoLength,
-                          char* errorBuffer, int errorCapacity, int* errorLength) {
+LIGHTER_SIGN_INFO_V2_EXPORT int SignModifyOrderInfoV2(int cMarketIndex, long long cIndex, long long cBaseAmount,
+                                                      long long cPrice, long long cTriggerPrice, uint8_t cSkipNonce,
+                                                      long long cNonce, int cApiKeyIndex, long long cAccountIndex,
+                                                      char* txInfoBuffer, int txInfoCapacity, int* txInfoLength,
+                                                      char* errorBuffer, int errorCapacity, int* errorLength) {
     lighter_ensure_large_sigaltstack();
     return SignModifyOrderInfoV2Go(cMarketIndex, cIndex, cBaseAmount, cPrice,
                                    cTriggerPrice, cSkipNonce, cNonce, cApiKeyIndex,
@@ -42,10 +42,10 @@ int SignModifyOrderInfoV2(int cMarketIndex, long long cIndex, long long cBaseAmo
                                    errorLength);
 }
 
-int SignCancelAllOrdersInfoV2(int cTimeInForce, long long cTime, uint8_t cSkipNonce,
-                              long long cNonce, int cApiKeyIndex, long long cAccountIndex,
-                              char* txInfoBuffer, int txInfoCapacity, int* txInfoLength,
-                              char* errorBuffer, int errorCapacity, int* errorLength) {
+LIGHTER_SIGN_INFO_V2_EXPORT int SignCancelAllOrdersInfoV2(int cTimeInForce, long long cTime, uint8_t cSkipNonce,
+                                                          long long cNonce, int cApiKeyIndex, long long cAccountIndex,
+                                                          char* txInfoBuffer, int txInfoCapacity, int* txInfoLength,
+                                                          char* errorBuffer, int errorCapacity, int* errorLength) {
     lighter_ensure_large_sigaltstack();
     return SignCancelAllOrdersInfoV2Go(cTimeInForce, cTime, cSkipNonce, cNonce,
                                        cApiKeyIndex, cAccountIndex, txInfoBuffer,
--- sharedlib/sign_info_v2.h
+++ sharedlib/sign_info_v2.h
@@ -7,29 +7,35 @@
 extern "C" {
 #endif
 
-int SignCreateOrderInfoV2(int cMarketIndex, long long cClientOrderIndex, long long cBaseAmount,
-                          int cPrice, int cIsAsk, int cOrderType, int cTimeInForce,
-                          int cReduceOnly, int cTriggerPrice, long long cOrderExpiry,
-                          uint8_t cSkipNonce, long long cNonce, int cApiKeyIndex,
-                          long long cAccountIndex, char* txInfoBuffer, int txInfoCapacity,
-                          int* txInfoLength, char* errorBuffer, int errorCapacity,
-                          int* errorLength);
+#ifdef _WIN32
+#define LIGHTER_SIGN_INFO_V2_EXPORT __declspec(dllexport)
+#else
+#define LIGHTER_SIGN_INFO_V2_EXPORT
+#endif
 
-int SignCancelOrderInfoV2(int cMarketIndex, long long cOrderIndex, uint8_t cSkipNonce,
-                          long long cNonce, int cApiKeyIndex, long long cAccountIndex,
-                          char* txInfoBuffer, int txInfoCapacity, int* txInfoLength,
-                          char* errorBuffer, int errorCapacity, int* errorLength);
+LIGHTER_SIGN_INFO_V2_EXPORT int SignCreateOrderInfoV2(int cMarketIndex, long long cClientOrderIndex, long long cBaseAmount,
+                                                      int cPrice, int cIsAsk, int cOrderType, int cTimeInForce,
+                                                      int cReduceOnly, int cTriggerPrice, long long cOrderExpiry,
+                                                      uint8_t cSkipNonce, long long cNonce, int cApiKeyIndex,
+                                                      long long cAccountIndex, char* txInfoBuffer, int txInfoCapacity,
+                                                      int* txInfoLength, char* errorBuffer, int errorCapacity,
+                                                      int* errorLength);
 
-int SignModifyOrderInfoV2(int cMarketIndex, long long cIndex, long long cBaseAmount,
-                          long long cPrice, long long cTriggerPrice, uint8_t cSkipNonce,
-                          long long cNonce, int cApiKeyIndex, long long cAccountIndex,
-                          char* txInfoBuffer, int txInfoCapacity, int* txInfoLength,
-                          char* errorBuffer, int errorCapacity, int* errorLength);
+LIGHTER_SIGN_INFO_V2_EXPORT int SignCancelOrderInfoV2(int cMarketIndex, long long cOrderIndex, uint8_t cSkipNonce,
+                                                      long long cNonce, int cApiKeyIndex, long long cAccountIndex,
+                                                      char* txInfoBuffer, int txInfoCapacity, int* txInfoLength,
+                                                      char* errorBuffer, int errorCapacity, int* errorLength);
 
-int SignCancelAllOrdersInfoV2(int cTimeInForce, long long cTime, uint8_t cSkipNonce,
-                              long long cNonce, int cApiKeyIndex, long long cAccountIndex,
-                              char* txInfoBuffer, int txInfoCapacity, int* txInfoLength,
-                              char* errorBuffer, int errorCapacity, int* errorLength);
+LIGHTER_SIGN_INFO_V2_EXPORT int SignModifyOrderInfoV2(int cMarketIndex, long long cIndex, long long cBaseAmount,
+                                                      long long cPrice, long long cTriggerPrice, uint8_t cSkipNonce,
+                                                      long long cNonce, int cApiKeyIndex, long long cAccountIndex,
+                                                      char* txInfoBuffer, int txInfoCapacity, int* txInfoLength,
+                                                      char* errorBuffer, int errorCapacity, int* errorLength);
+
+LIGHTER_SIGN_INFO_V2_EXPORT int SignCancelAllOrdersInfoV2(int cTimeInForce, long long cTime, uint8_t cSkipNonce,
+                                                          long long cNonce, int cApiKeyIndex, long long cAccountIndex,
+                                                          char* txInfoBuffer, int txInfoCapacity, int* txInfoLength,
+                                                          char* errorBuffer, int errorCapacity, int* errorLength);
 
 int SignCreateOrderInfoV2Go(int cMarketIndex, long long cClientOrderIndex, long long cBaseAmount,
                             int cPrice, int cIsAsk, int cOrderType, int cTimeInForce,
